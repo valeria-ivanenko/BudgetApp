07818737736bdfef910284a7f9f614ea8bcbf534
valeria-ivanenko <98172492+valeria-ivanenko@users.noreply.github.com>
2022-01-21 19:19:12 +0200

    Add files via upload

---
+++ BudgetApp/BudgetApp/Budget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BudgetApp
+{
+    public class Budget
+    {
+        List<double> transactions;
+        public void AddTransaction(double transaction)
+        {
+            transaction.Add(transaction);
+        }
+    }
+}
---
+++ BudgetApp/BudgetApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BudgetApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = new Budget();
+            budget.AddTransaction(89.1);
+            List<double> transactions = new List<double> { 27.3, 4.8, 384.486 };
+            var result = 0.0;
+            foreach (double transaction in transactions) {
+                result += transaction;
+            }
+            Console.WriteLine(result);
+            result /= transactions.Count;
+            Console.WriteLine($"The average transaction is ${result}");
+        }
+        
+
+    }
+}
